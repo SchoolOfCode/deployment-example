65db746afd798a44ce304287963862fb10e2933d
Dave Adams <dave@schoolofcode.co.uk>
2022-01-11 15:56:52 +0000

    Fetch bad joke

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const [joke, setJoke] = useState("");
+
+  useEffect(() => {
+    async function getJoke() {
+      const res = await fetch("https://icanhazdadjoke.com/", {
+        headers: { accept: "application/json" },
+      });
+      const data = await res.json();
+      console.log(data);
+      setJoke(data.joke);
+    }
+    getJoke();
+  }, []);
+
   return (
     <div className="App">
       <h1>My first useEffect fetch!</h1>
+      <p>{joke}</p>
     </div>
   );
 }
